7a16481b4e13f7b99f43455a531dd0c59c3fc043
User <rrk@gmail.com>
2023-01-08 00:52:45 +0600

    carousel slider using swiper package

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:store_app_api/constants/global_colors.dart';
 import 'package:store_app_api/widgets/appbar_icons.dart';
+import 'package:store_app_api/widgets/sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,69 +13,89 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   late TextEditingController _textEditingController;
 
   @override
   void initState() {
-    _textEditingController=TextEditingController();
+    _textEditingController = TextEditingController();
     super.initState();
   }
+
   @override
   void dispose() {
     _textEditingController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
-         FocusScope.of(context).unfocus();
+        FocusScope.of(context).unfocus();
       },
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Home"),
           leading: AppbarIcons(
-            function: (){},
+            function: () {},
             icon: IconlyBold.category,
           ),
           actions: [
             AppbarIcons(
-              function: () {  },
+              function: () {},
               icon: IconlyBold.user3,
-
             )
           ],
         ),
-        body:Padding(
+        body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-          children: [
-            const SizedBox(height: 18,),
-            TextField(
-            controller: _textEditingController,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                hintText: "Search",
-                filled: true,
-                fillColor: Theme.of(context).cardColor,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(
-                    color: Theme.of(context).cardColor
-                  )
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(
-                      width: 1,
-                        color: Theme.of(context).colorScheme.secondary
-                    )
-                ),
-                suffixIcon: Icon(Icons.search,color: lightIconsColor,)
+            children: [
+              const SizedBox(
+                height: 18,
               ),
-            )
-          ],
+              TextField(
+                controller: _textEditingController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    filled: true,
+                    fillColor: Theme.of(context).cardColor,
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide:
+                            BorderSide(color: Theme.of(context).cardColor)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: Theme.of(context).colorScheme.secondary)),
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: lightIconsColor,
+                    )),
+              ),
+              SizedBox(
+                height: size.height * 0.25,
+                child: Swiper(
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return const SaleWidget();
+                  },
+                  pagination: const SwiperPagination(
+                    alignment: Alignment.bottomCenter,
+                    builder: DotSwiperPaginationBuilder(
+                      activeColor: Colors.red,
+                      color: Colors.white,
+                    ),
+
+                  ),
+                  autoplay: true,
+                  // control: SwiperControl(),
+                ),
+              ),
+            ],
           ),
         ),
       ),
---
+++ lib/widgets/sale_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+class SaleWidget extends StatelessWidget {
+  const SaleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size=MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        gradient: const LinearGradient(
+          colors: [
+            Color(0xFF7A60A5),
+            Color(0xFF82C3DF)
+          ],
+          begin: FractionalOffset(0.0, 0.0),
+          end: FractionalOffset(1.0, 0.0),
+          stops: [0.0,1.0],
+          tileMode: TileMode.clamp
+        )
+      ),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+
+                  borderRadius: BorderRadius.circular(18),
+                        color: const Color(0xFF9689CE),
+
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    // mainAxisSize: MainAxisSize.min,
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children:const [
+                      Text("Get the special discount",style: TextStyle(
+                        color: Colors.white,
+                      ),),
+                       SizedBox(height: 18,),
+                      Flexible(
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Text("50%\nOFF",style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                            ),),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Image.network(
+
+                  'https://tse4.mm.bing.net/th?id=OIP.d-7UFbAaPsT2y3dYpaKm1AHaFb&pid=Api&P=0',
+                width: double.infinity,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
